f8bc136066826cfc062df137d4df165511b419a5
Lynn M <78457709+Giggles999@users.noreply.github.com>
2021-10-06 17:54:52 -0700

    updated metric/imperial

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -20,7 +20,7 @@ export default function WeatherForecast(props) {
     let apiKey = "6f65ac3695a44ef64022cd653378b553";
     let longitude = props.coordinates.lon;
     let latitude = props.coordinates.lat;
-    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=imperial`;
 
     axios.get(apiUrl).then(handleResponse);
   }
