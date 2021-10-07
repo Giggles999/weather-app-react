b9f29fb2e2b7b4467808710df1360e1e42aac4e0
Lynn M <78457709+Giggles999@users.noreply.github.com>
2021-10-06 18:49:03 -0700

    update dates

--- src/FormattedDate.js
+++ src/FormattedDate.js
@@ -1,7 +1,8 @@
 import React from "react";
 
 export default function FormattedDate(props) {
-  let days = [
+    let date = new Date(props);
+    let days = [
     "Sunday",
     "Monday",
     "Tuesday",
@@ -10,19 +11,46 @@ export default function FormattedDate(props) {
     "Friday",
     "Saturday"
   ];
+  
+  let months = [
+    "January", 
+    "Febuary", 
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+    ];
+  
+  
   let day = days[props.date.getDay()];
+  let month = months[props.date.getMonth()]
   let hours = props.date.getHours();
-  if (hours < 10) {
-    hours = `0${hours}`;
+  let amOrPm ="AM";
+  let numberDay = date.getDate();
+    
+  
+  
+  
+  if (hours >= 12) {
+    amOrPm = "PM";
+    hours = hours - 12;
   }
   
   let minutes = props.date.getMinutes();
   if (minutes < 10) {
     minutes = `0${minutes}`;
   }
+
+
   return (
     <div>
-      {day} {hours}:{minutes}
+      {day} {month} {numberDay},  {hours}:{minutes}{amOrPm}
     </div>
   );
 }
